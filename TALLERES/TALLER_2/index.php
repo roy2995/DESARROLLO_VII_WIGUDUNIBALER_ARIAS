e5c7545deed98c720b04adeaa2c56c86f713278c
Wigudunibaler <wigudunibalera@gmail.com>
2024-08-20 19:33:59 -0500

    Ejercicio práctico: combinación de conceptos aprendidos

--- TALLERES/TALLER_2/index.php
+++ TALLERES/TALLER_2/index.php
@@ -1,21 +1,30 @@
 
 <?php
+// Definición de variables
+$nombre = "María";
+$edad = 30;
+$ciudad = "Madrid";
 
-$nombre = "Juan";
-$edad = 25;
+// Definición de constante
+define("PROFESION", "Ingeniera");
 
-// Usando echo
-echo "Hola, mundo!<br>";
-echo "Mi nombre es $nombre<br>";
+// Creación de mensaje usando diferentes métodos de concatenación e impresión
+$mensaje1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+$mensaje2 = "Vivo en $ciudad y soy " . PROFESION . ".";
 
-// Usando print
-print "Tengo $edad años<br>";
+echo $mensaje1 . "<br>";
+print($mensaje2 . "<br>");
 
-// Usando printf (permite formateo)
-printf("Me llamo %s y tengo %d años<br>", $nombre, $edad);
+printf("En resumen: %s, %d años, %s, %s<br>", $nombre, $edad, $ciudad, PROFESION);
 
-// Usando var_dump (útil para debugging)
+echo "<br>Información de debugging:<br>";
 var_dump($nombre);
+echo "<br>";
+var_dump($edad);
+echo "<br>";
+var_dump($ciudad);
+echo "<br>";
+var_dump(PROFESION);
 echo "<br>";
 ?>
                         
